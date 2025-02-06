2b0aa4e8e167d21b00e01c6c90625578549ebe6d
Mike <mickael.ha@gmail.com>
2025-02-06 14:30:56 +0100

    Modifcation parametre CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://mon-projet-pokemon.surge.sh",
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   })
